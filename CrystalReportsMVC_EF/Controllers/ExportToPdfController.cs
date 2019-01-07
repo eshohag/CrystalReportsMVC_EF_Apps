1cb708e128fd3d488aa2f936a2bc45f3325a93bb
mdshohagmia <shohaghassan14@hotmail.com>
2019-01-07 13:17:14 +0600

    Done all Task

--- CrystalReportsMVC_EF/Controllers/ExportToPdfController.cs
+++ CrystalReportsMVC_EF/Controllers/ExportToPdfController.cs
@@ -12,9 +12,9 @@ namespace CrystalReportsMVC_EF.Controllers
         // GET: ExportToPdf
         public ActionResult Export()
         {
-            ReportDocument rd = new ReportDocument();
-            rd.Load(Path.Combine(Server.MapPath("~/CrystalReports/ProductsCrystalReport.rpt")));
-            rd.SetDataSource(db.Products.Select(p => new
+            ReportDocument aReportDocument = new ReportDocument();
+            aReportDocument.Load(Path.Combine(Server.MapPath("~/CrystalReports/ProductsCrystalReport.rpt")));
+            aReportDocument.SetDataSource(db.Products.Select(p => new
             {
                 Id = p.Id,
                 Name = p.Name,
@@ -25,14 +25,30 @@ namespace CrystalReportsMVC_EF.Controllers
             Response.Buffer = false;
             Response.ClearContent();
             Response.ClearHeaders();
-            Stream aStream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            Stream aStream = aReportDocument.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
             aStream.Seek(0, SeekOrigin.Begin);
             return File(aStream, "application/pdf", "ProductList.pdf");
         }
 
         public ActionResult ExportToGroupBy()
         {
-            return View();
+            ReportDocument aReportDocument = new ReportDocument();
+            aReportDocument.Load(Path.Combine(Server.MapPath("~/CrystalReports/ProductGroupingCrystalReport.rpt")));
+            aReportDocument.SetDataSource(db.Products.Select(p => new
+            {
+                Id = p.Id,
+                Name = p.Name,
+                CategoryId = p.CategoryId,
+                CategoryName = p.Category.Name,
+                Quentity = p.Quentity,
+                Price = p.Price
+            }).ToList());
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Stream aStream = aReportDocument.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            aStream.Seek(0, SeekOrigin.Begin);
+            return File(aStream, "application/pdf", "ProductList.pdf");
         }
     }
 }
