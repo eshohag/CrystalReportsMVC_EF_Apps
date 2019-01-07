8c6a26b82d2a5e9072aa5e23ca43193f83022dfa
mdshohagmia <shohaghassan14@hotmail.com>
2019-01-07 10:14:28 +0600

    Add project files.

---
+++ CrystalReportsMVC_EF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrystalReportsMVC_EF.Startup))]
+namespace CrystalReportsMVC_EF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
